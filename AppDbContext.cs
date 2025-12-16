900c65e0ef2076633bd10cd55884eec6e274b236
jafeeradel-cloud <jafeeradel@gmail.com>
2025-12-17 01:14:10 +0300

    Update AppDbContext.cs

--- AppDbContext.cs
+++ AppDbContext.cs
@@ -5,12 +5,9 @@ namespace RealEstateApi.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options)
-            : base(options)
-        {
-        }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<Property> Properties { get; set; }
-        public DbSet<User> Users { get; set; }   // 🔴 هذا السطر المهم
+        public DbSet<User> Users { get; set; }
     }
 }
