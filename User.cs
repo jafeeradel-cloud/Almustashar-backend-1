f4cea509e33001969d3c247ae95afc8b60d82f0b
jafeeradel-cloud <jafeeradel@gmail.com>
2025-12-17 01:18:16 +0300

    Update User.cs

--- User.cs
+++ User.cs
@@ -8,9 +8,13 @@ namespace RealEstateApi.Models
         public int Id { get; set; }
 
         [Required]
-        public string Name { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+
+        public string? PhoneNumber { get; set; }
+
+        public string? Email { get; set; }
 
         [Required]
-        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
     }
 }
